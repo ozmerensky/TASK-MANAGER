2716b56a6e9b15cc10ed2ab0bf2ea982ef0f7f18
Oz Merensky <oz.meresnky@gmail.com>
2026-01-10 10:53:17 +0200

    added dockers and updated configurations

--- backend/src/startServer.ts
+++ backend/src/startServer.ts
@@ -1,34 +1,19 @@
-import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import cors from 'cors';
+import app from './app';
 
 dotenv.config();
 
-const app = express();
-
-app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('API is working!');
-});
-
-import taskRoutes from './routes/taskRoutes';
-app.use('/tasks', taskRoutes);
-
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI!)
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/tasks';
+
+mongoose.connect(MONGO_URI)
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
-      console.log(`connected to the MongoDB ${process.env.MONGO_URI}`);
+      console.log(`Connected to MongoDB at ${MONGO_URI}`);
     });
   })
   .catch(err => console.error(err));
+
