51fc69c3504a15c9c2839ac5fa4bd70e5245cd5e
Oz Merensky <oz.meresnky@gmail.com>
2026-06-19 12:42:02 +0300

    test: stabilize edit, toggle, and delete chains and implement app warm-up

--- frontend/cypress/e2e/standard-flow.cy.ts
+++ frontend/cypress/e2e/standard-flow.cy.ts
@@ -2,6 +2,10 @@ import tasksFunctions from "../support/funcitons/tasksFunctions"
 import apiRequests from "../support/funcitons/apiFunctions/apiRequests"
 
 describe('Standard User Task Flow', () => {
+    before(() => {
+        tasksFunctions.appEntrance();
+    });
+
     it('Should create a task by typing into the form and validate DB', () => {
         cy.fixture('tasks').then((tasks) => {
             tasksFunctions.appEntrance();
@@ -10,9 +14,8 @@ describe('Standard User Task Flow', () => {
             tasksFunctions.fillManualTaskForm(tasks.created);
             tasksFunctions.grabFormValues().then((task) => {
                 tasksFunctions.submitCreateForm();
-                apiRequests.waitForTaskCreationAndGetId();
-                tasksFunctions.validateCardValues(task);
-                cy.then(() => {
+                apiRequests.waitForTaskCreationAndGetId().then(() => {
+                    tasksFunctions.validateCardValues(task);
                     apiRequests.validateTaskInDB(task);
                 });
             });
@@ -32,46 +35,46 @@ describe('Standard User Task Flow', () => {
 
         it('Should edit last task and validate the update', () => {
             cy.fixture('tasks').then((tasks) => {
-                tasksFunctions.appEntrance()
-                tasksFunctions.validateMainTitle()
-                apiRequests.interceptUpdateTask()
-                tasksFunctions.openEditFormOnLastTask()
-                tasksFunctions.editTask()
-                tasksFunctions.grabTaskValues().then(() => {
-                    tasksFunctions.fillManualTaskEditForm(tasks.edited)
-                    tasksFunctions.grabTaskValues().then((newTask: any) => {
-                        tasksFunctions.saveEditTask()
-                        apiRequests.waitForTaskEditAndGetId()
-                        tasksFunctions.validateCardValues(newTask)
-                        apiRequests.validateTaskInDB(newTask)
-                    })
-                })
-            })
-        })
+                tasksFunctions.appEntrance();
+                tasksFunctions.validateMainTitle();
+                apiRequests.interceptUpdateTask();
+                tasksFunctions.openEditFormOnLastTask();
+                tasksFunctions.editTask();
+                tasksFunctions.fillManualTaskEditForm(tasks.edited);
+                tasksFunctions.grabTaskValues().then((newTask: any) => {
+                    tasksFunctions.saveEditTask();
+                    apiRequests.waitForTaskEditAndGetId().then(() => {
+                        tasksFunctions.validateCardValues(newTask);
+                        apiRequests.validateTaskInDB(newTask);
+                    });
+                });
+            });
+        });
 
         it('Should search and toggle completion status of last task and validate the update', () => {
-            tasksFunctions.appEntrance()
-            tasksFunctions.validateMainTitle()
-            apiRequests.interceptUpdateTask()
-            tasksFunctions.openEditFormOnLastTask()
-            tasksFunctions.searchTaskByTitle()
+            tasksFunctions.appEntrance();
+            tasksFunctions.validateMainTitle();
+            apiRequests.interceptUpdateTask();
+            tasksFunctions.openEditFormOnLastTask();
+            tasksFunctions.searchTaskByTitle();
             tasksFunctions.toggleCompletionOnFoundTask();
-            apiRequests.waitForTaskEditAndGetId();
-            tasksFunctions.validateToggleChangedStatus();
-            apiRequests.validateCompletedTask()
-        })
+            apiRequests.waitForTaskEditAndGetId().then(() => {
+                tasksFunctions.validateToggleChangedStatus();
+                apiRequests.validateCompletedTask();
+            });
+        });
 
         it('Should find and delete the last task', () => {
-            tasksFunctions.appEntrance()
-            tasksFunctions.validateMainTitle()
-            apiRequests.interceptDeleteTask()
-            tasksFunctions.openEditFormOnLastTask()
-            tasksFunctions.searchTaskByTitle()
-            tasksFunctions.deleteTask()
-            apiRequests.waitForTaskDeleteAndGetId()
-            apiRequests.validateDeletedTask()
-        })
-    })
-})
-
-
+            tasksFunctions.appEntrance();
+            tasksFunctions.validateMainTitle();
+            apiRequests.interceptDeleteTask();
+            tasksFunctions.openEditFormOnLastTask();
+            tasksFunctions.searchTaskByTitle();
+            tasksFunctions.deleteTask();
+            
+            apiRequests.waitForTaskDeleteAndGetId().then(() => {
+                apiRequests.validateDeletedTask();
+            });
+        });
+    });
+});
